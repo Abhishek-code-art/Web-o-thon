221ae48836f4b4af3a2ec7021aaa207e61c70213
Abhishek <8439tabi@gmail.com>
2023-08-06 00:31:26 +0530

    set up path for views folder in app.js file.

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.set('views', path.join(__dirname,'views'));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res) {
     res.render("home", {title: "Cesta"});
